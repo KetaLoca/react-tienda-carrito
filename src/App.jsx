4a70c935aa0ab88d9b93e480e17914cdfe209866
KetaLoca <mianramar@gmail.com>
2024-06-21 13:19:10 +0200

    Lógica useFilters extraída a un customHook

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,8 @@ import { products as initialProducts } from "./mocks/products.json";
 import { Products } from "./components/Products";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
-import { FiltersContext } from "./context/filters";
+import { useFilters } from "./hooks/filtros";
 
-function useFilters() {
-  // const [filters, setFilters] = useState({ category: "all", minPrice: 0 });
-  const [filters, setFilters] = useContext({FiltersContext});
-
-  const filterProducts = (products) => {
-    return products.filter((product) => {
-      return (
-        product.price >= filters.minPrice &&
-        (filters.category == "all" || product.category == filters.category)
-      );
-    });
-  };
-  return { filterProducts, setFilters};
-}
 
 function App() {
   const [products] = useState(initialProducts);
--- src/context/filters.jsx
+++ src/context/filters.jsx
@@ -1,9 +1,10 @@
 import { createContext, useState } from "react";
 
+//Constante que debemos consumir para recuperar el State
 export const FiltersContext = createContext();
 
 export function FiltersProvider({ children }) {
-  const [filters, setFilters ] = useState({ category: "all", minPrice: 0 });
+  const [filters, setFilters] = useState({ category: "all", minPrice: 0 });
 
   return (
     <FiltersContext.Provider
