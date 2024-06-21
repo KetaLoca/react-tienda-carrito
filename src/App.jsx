7c188b390325804426c6f1b1c74cfd29ccc13914
mianramar <mianramar@gmail.com>
2024-06-21 09:21:22 +0200

    Lógica de filtro añadida

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import { products } from "./mocks/products.json";
+import { products as initialProducts } from "./mocks/products.json";
 import { Products } from "./components/Products";
 
 function App() {
+  const [products] = useState(initialProducts);
+  const [filters, setFilters] = useState({ category: "all", minPrice: 0 });
+  const filterProducts = (products) => {
+    return products.filter((product) => {
+      return (
+        product.price >= filters.minPrice &&
+        (filters.category == "all" || product.category == filters.category)
+      );
+    });
+  };
   return <Products products={products} />;
 }
 
