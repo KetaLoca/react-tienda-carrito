e338bad77fd66f4cb30ed0f7a39d4a13542a487a
mianramar <mianramar@gmail.com>
2024-06-21 09:04:17 +0200

    Ya se muestra el grid con los productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { products } from "./mocks/products.json";
+import { Products } from "./components/Products";
 
 function App() {
-
-  return (
-    <h1>Shoppin cart</h1>
-  )
+  return <Products products={products} />;
 }
 
-export default App
+export default App;
