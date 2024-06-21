0300f34465896b831540b1cb9b539f8986114294
KetaLoca <mianramar@gmail.com>
2024-06-21 10:54:56 +0200

    Pequeño cambio para probar el commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Filters } from "./Filters.jsx";
 export function Header({onChange}) {
   return (
     <header>
-      <h1>React Shop 🛒</h1>
+      <h1>REACT SHOP🛒</h1>
       <Filters onChange={onChange} />
     </header>
   );
